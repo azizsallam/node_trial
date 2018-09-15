caa3392f5a8ac466db7ff492527ecfdd2ea1d9c2
عبدالعزيز سلام <aziz.sallam99hotmail.com>
2018-09-15 20:21:14 +0200

    yy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res) {
-       var  skills =["MVC", "C#",  "Node  JS", "mkyy" ,  "Python" ,  "IDE visual studio","Mssql"];
+       var  skills =["MVC", "C#", "mmm","mlll","moo","muuu" ,"mdd","mkkkk","Node  JS", "mkyy" ,  "Python" ,  "IDE visual studio","Mssql"];
        res.render('index', {
        title : "Select technology",
        skills:skills
