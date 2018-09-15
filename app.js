43a45abe4260d9e218d353e055c19a7d297ca613
عبدالعزيز سلام <aziz.sallam99hotmail.com>
2018-09-15 05:24:23 +0200

    ii

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 ///////////////////  the next line how to call bootstrap files in yuor view ///////////////////
 app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 //////////////////////////////////
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
+/*esversion=6 */
 var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res) {
- var  skills =["MVC", "C#",  "Node JS",    "Python" ,  "IDE visual studio","Mssql"];
-
-  res.render('index', {
+       var  skills =["MVC", "C#",  "Node  JS", "mkyy" ,  "Python" ,  "IDE visual studio","Mssql"];
+       res.render('index', {
        title : "Select technology",
-       skills: skills
+       skills:skills
+   });
   });
-  
-});
-
-router.post("/Skill" , function(req, res){
-        res.render('Skill',{
-       skill: req.body.dropDown
+ 
+ router.post("/Skill" , function(req, res){
+       console.log(req.body);
+       res.render('Skill',{
+       skill: req.body.dropdown
    });
 });
 
