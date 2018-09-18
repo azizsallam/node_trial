2de535500b61e27fbf17381c0b9ac8a8cc6ec2dc
azizsallam <aziz.sallam99hotmail.com>
2018-09-19 00:04:12 +0200

    yy

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,25 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res) {
- var  skills =["MVC", "C#",  "Node JS",    "Python" ,  "IDE visual studio","Mssql"];
-
-  res.render('index', {
+   var  skills =["MVC", "C#",  "Node JS",    "Python" ,  "IDE visual studio","Mssql"];
+   res.render('index', {
        title : "Select technology",
-       skills: skills
+       skills:skills ,
+       sobo:""
   });
-  
+ });
+router.get("/skilla", function (req, res) {
+    console.log(pppppppppppp) ;
+    var  skills =["MVC", "C#",  "Node JS",    "Python" ,  "IDE visual studio","Mssql"];
+     res.render('index', {
+        title : "Select technology",
+        skills:skills ,
+        sobo:""
+   });
 });
 
-router.post("/Skill" , function(req, res){
+ router.post("/Skill" , function(req, res){
+   
         res.render('Skill',{
        skill: req.body.dropDown
    });
